079cb217c68ee1ed739b4aa841d9da4182435f04
eugene <eugenemugiraneza2@gmail.com>
2023-03-02 18:45:50 +0200

    add close and open button

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,4 +1,5 @@
 
+import React from 'react'
 import Logo from '../components/images/logo/download.png'
 const Header = (Header) => {
     const logo = {
@@ -16,6 +17,12 @@ const Header = (Header) => {
     const navbar ={
         fontWeight: '600px'
     }
+
+      const [isOpen, setisOpen] = React.useState(false);
+    
+      function handleClick() {
+        setisOpen(!isOpen);
+      }
     return (
         <div>
         
@@ -28,15 +35,20 @@ const Header = (Header) => {
       </span>
     </div>
     <div className="block lg:hidden">
-      <button className="flex items-center px-3 py-2 border rounded text-black border-[#232323] hover:text-[#05C605] hover:border-[#05C605]">
-        <svg className="fill-current texiblue-300 h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      <button onClick={handleClick} className="flex items-center px-3 py-2 border rounded text-black border-[#232323] hover:text-[#05C605] hover:border-[#05C605]">
+      {isOpen && ( <svg className="fill-current texiblue-300 h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      
+      )}
+      {!isOpen && (
+        <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+        )}
       </button>
     </div>
     <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
       <div className="text-sm lg:flex-grow">
       
       </div>
-      <div className="navbar">
+      <div className={`lg:flex ${  isOpen ? "md:block" : "hidden" } `}>
       <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-[#05C605] hover:text-[16px] hover:text-[#05C605] mr-10 duration-300">
       <b>Home</b>
         </a>
