bffd6101fae135b913de00beef9b7d509775319b
eugizzo <eugenemugiraneza2@gmail.com>
2023-03-29 17:37:56 +0200

    login update

--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -1,22 +1,63 @@
-import React, {useState} from "react"; 
+import React, { useState } from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import { FaCircle } from "react-icons/fa";
 
 const data = [
   {
-   image: require('./images/loginCarousel/monitoring.png'), 
-   caption:"Smart City Waste Monitoring",
-   description:"AI & IoT Empowered system for monitoring and facilitating waste gathering and collection in real time"
+    image: require('./images/loginCarousel/ill08.png'),
+    caption: "Smart City Waste Monitoring",
+    description: "AI & IoT Empowered system for monitoring and facilitating waste gathering and collection in real time",
+    dot1: <FaCircle className="text-[#05C605]" />,
+    dot2: <FaCircle className="text-[#cccccc]" />,
+    dot3: <FaCircle className="text-[#cccccc]" />,
+  },
+  
+  {
+    image: require('./images/loginCarousel/ill02.png'),
+    caption: "Automatically Scheduled Waste Collection",
+    description: "Waste Levels are monitored and an automatic schedule for collection is made on time",
+    dot1: <FaCircle className="text-[#cccccc]" />,
+    dot2: <FaCircle className="text-[#05C605]" />,
+    dot3: <FaCircle className="text-[#cccccc]" />,
+
   },
   {
-    image:require('./images/loginCarousel/ill02.png'), 
-    caption:"Automatically Scheduled Waste Collection",
-    description:"Waste Levels are monitored and an automatic schedule for collection is made on time"
-   },
+    image: require('./images/loginCarousel/ill03.png'),
+    caption: "Waste Gathering and  Collection Analytics",
+    description: "Collection activities are analyzed in real time to give location based insights",
+    dot1: <FaCircle className="text-[#cccccc]" />,
+    dot2: <FaCircle className="text-[#cccccc]" />,
+    dot3: <FaCircle className="text-[#05C605]" />,
+
+  },
+ 
+  {
+    image: require('./images/loginCarousel/ill08.png'),
+    caption: "Smart City Waste Monitoring",
+    description: "AI & IoT Empowered system for monitoring and facilitating waste gathering and collection in real time",
+    dot1: <FaCircle className="text-[#05C605]" />,
+    dot2: <FaCircle className="text-[#cccccc]" />,
+    dot3: <FaCircle className="text-[#cccccc]" />,
+  },
+  {
+    image: require('./images/loginCarousel/ill02.png'),
+    caption: "Automatically Scheduled Waste Collection",
+    description: "Waste Levels are monitored and an automatic schedule for collection is made on time",
+    dot1: <FaCircle className="text-[#cccccc]" />,
+    dot2: <FaCircle className="text-[#05C605]" />,
+    dot3: <FaCircle className="text-[#cccccc]" />,
+
+  }, 
    {
-    image:require('./images/loginCarousel/ill03.png'), 
-    caption:"Waste Gathering and Collection Analytics",
-    description:"Collection activities are analyzed in real time to give location based insights"
-   } 
+    image: require('./images/loginCarousel/ill03.png'),
+    caption: "Waste Gathering and  Collection Analytics",
+    description: "Collection activities are analyzed in real time to give location based insights",
+    dot1: <FaCircle className="text-[#cccccc]" />,
+    dot2: <FaCircle className="text-[#cccccc]" />,
+    dot3: <FaCircle className="text-[#05C605]" />,
+
+  }
+
 ]
 
 function HomeCarousel() {
@@ -24,27 +65,33 @@ function HomeCarousel() {
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
-
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-       {data.map((slide, i) => {
-        return (
-          <Carousel.Item>        
-        <img
-          className="w-100"
-          src={slide.image}
-          alt="slider image"
-        />
-        <Carousel.Caption className="pt-[70px]">
-          <h3 className="text-black">{slide.caption}</h3>
-          <p className="text-black">{slide.description}</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      
-        )
-      })}
-      
-    </Carousel>
+    <div>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        {data.map((slide,i) => {
+          return (
+            <Carousel.Item>
+              <img
+                className="w-[200px]"
+                src={slide.image}
+                alt="slider image"
+              />
+              <h3 className="text-black text-xl font-bold m-1">{slide.caption}</h3>
+              <p className="text-gray-600">{slide.description}</p>
+              <div className="flex pt-4 justify-center">
+                <p className="text-[10px]">{slide.dot1}</p>
+                <p className="pl-1 pr-1 text-[10px]">{slide.dot2}</p>
+                <p className="text-[10px]">{slide.dot3}</p>
+              </div>
+            </Carousel.Item>
+
+          )
+        })}
+        <div>
+        </div>
+
+      </Carousel>
+    </div>
   );
 }
 export default HomeCarousel;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import Header from '../layout/Header'
 import { FaEyeSlash,FaEye } from "react-icons/fa";
-import Carousel from './HomeCarousel';
 import HomeCarousel from './HomeCarousel';
+import '../layout/header.css'
 
 const Login = () => {
 
@@ -31,15 +31,14 @@ const Login = () => {
     <div>
      <Header className="" />
 
-
-     <div className='container'>
+     <div className='container overflow-auto'>
        <div className='lg:flex'>
        <div className='lg:w-1/6'></div>
-         <div className='lg:w-2/6 mt-52 mr-20'>
+         <div className='lg:w-2/6 lg:mt-52 mt-20 lg:mr-20 m-12 flex justify-center'>
          <HomeCarousel />
          </div>
-         <div className='lg:w-3/6 mr-20 '>
-           <form onClick={onSubmit} class="bg-white shadow-md rounded-2xl  mt-52 px-8 pt-6 pb-8 mb-4">
+         <div className='lg:w-3/6 lg:mr-52 '>
+           <form onClick={onSubmit} class="bg-white shadow-md rounded-2xl  lg:mt-52  mt-8 px-8 pt-6 pb-8 mb-4">
              <h2 className='text-sm mt-2 mb-2 '>login</h2>
                
             
@@ -47,8 +46,8 @@ const Login = () => {
                
                <input class="bg-[#fafbfd] form-control hover:border-green-500 hover:border-2" id="email" type="text" placeholder='Email Address or phone number' />
              </div>
-             <div class="input-group mb-4 bg-[#e8f0fe] focus:border-green-500 border hover:border-2 flex justify-between  py-2">
-               <input type={passwordType} onChange={handlePasswordChange} value={passwordInput} name="password" class="border-none outline-none bg-[#e8f0fe] ml-2" placeholder="Password" />
+             <div class="input-group mb-4 bg-[#fafbfd] rounded focus:border-green-500 border hover:border-2 flex justify-between  py-2">
+               <input type={passwordType} onChange={handlePasswordChange} value={passwordInput} name="password" class="border-none outline-none bg-[#fafbfd] ml-2" placeholder="Password" />
                     
                      <button className="mr-4 " onClick={togglePassword}>
                      { passwordType==="password"? <FaEyeSlash/> :<FaEye/> }
@@ -58,9 +57,9 @@ const Login = () => {
                <button class=" w-full bg-[#05C605] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                  Login
                </button>
-               <div className='flex'>
-               <h6 className='mt-4 ml-20'>Can't login?</h6>
-               <h6 className='mt-4  pl-4 pr-4'>-</h6>
+               <div className='flex justify-around'>
+               <h6 className='mt-4 ml-8'>Can't login?</h6>
+               <h6 className='mt-4  '>-</h6>
                <h6 className=' mt-4 text-[#05C605]'>Login as SuperAdmin</h6>
                </div>
              </div>
